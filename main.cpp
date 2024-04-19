1698589f67c737849702755b35c74951ebc5b036
Derrity <86703464+Derrity@users.noreply.github.com>
2024-04-20 00:43:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@
 #include <future>
 #include <vector>
 #include <fstream>
-#include <GenerateMnemonic.h>
-#include <ProcessAddress.h>
+#include "generateAddress/main.hpp"
 #include <json.hpp>
 
 int successCount = 0;
